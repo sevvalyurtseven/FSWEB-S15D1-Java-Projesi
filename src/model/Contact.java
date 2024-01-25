8e301e27d1dc9d50845ff0d536d98a8d983cd8a8
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-25 08:27:05 +0300

    toString eklendi

--- src/model/Contact.java
+++ src/model/Contact.java
@@ -24,4 +24,12 @@ public class Contact {
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
     }
+
+    @Override
+    public String toString() {
+        return "Contact{" +
+                "name='" + name + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
