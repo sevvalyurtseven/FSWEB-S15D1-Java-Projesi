920a59eb366a10975b4906bba220c5ebbd87dd70
sevvalyurtseven <aysesevvalyurtseven@hotmail.com>
2024-01-25 08:30:50 +0300

    constructor eklendi

--- src/model/MobilePhone.java
+++ src/model/MobilePhone.java
@@ -5,4 +5,9 @@ import java.util.ArrayList;
 public class MobilePhone {
     private String myNumber;
     private ArrayList<Contact> myContacts;
+
+    public MobilePhone(String myNumber) {
+        this.myNumber = myNumber;
+        this.myContacts = new ArrayList<>(); //artik bos bir arraylist'im var. Rahatca ekleme cıkarma yapabilcez
+    }
 }
